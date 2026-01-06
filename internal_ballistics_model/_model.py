b6fb600f12659417fb17a16f8ce93893f2237fa4
brandon.schroder <brandonschroder1@gmail.com>
2026-01-06 10:23:13 +0200

    Remove redundant imports from _model.py and _example.py

--- internal_ballistics_model/_model.py
+++ internal_ballistics_model/_model.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 
 # Import functions
--- level_set_model/_example.py
+++ level_set_model/_example.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 from pathlib import Path
 
 # Import our new modules
