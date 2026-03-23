083cf901005aa05bf7d4e162d866b189f32ebcd3
brandon.schroder <bs@mkaerospace.co.za>
2026-03-23 08:31:10 +0200

    Add geometry viewing script for debugging

---
+++ test/geometry/view_geom.py
@@ -0,0 +1,9 @@
+import pyvista as pv
+
+propellant = pv.read("NAWC6-SRM-Propellant.STL")
+casing = pv.read("NAWC6-SRM-Casing.STL")
+
+plotter = pv.Plotter()
+plotter.add_mesh(propellant, color="red", opacity=0.8)
+plotter.add_mesh(casing, color="blue", opacity=0.8)
+plotter.show()
