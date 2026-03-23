07e5c66c2270b9e91c043cbd0e16d90ddb3957d4
brandon.schroder <bs@mkaerospace.co.za>
2026-03-23 08:30:04 +0200

    Add NAWC6 test case

--- _example.py
+++ _example.py
@@ -6,31 +6,38 @@ import coupled_solver_model
 import internal_ballistics_model
 import level_set_model
 
-bounds=[10.0*1e-3, 35.0*1e-3, None, None, 0.0*1e-3, 918.5*1e-3] # 1035.5
-prop_file = Path("test\\geometry\\07R-SRM-Propellant.STL")
-case_file = Path("test\\geometry\\07R-SRM-Casing.STL")
+bounds=[13.0*1e-3, 70.0*1e-3, None, None, 0.0*1e-3, 1899*1e-3] # 1035.5
+prop_file = Path("test\\geometry\\NAWC6-SRM-Propellant.STL")
+case_file = Path("test\\geometry\\NAWC6-SRM-Casing.STL")
 
 ib_config = internal_ballistics_model.config.SimulationConfig(
     # Grid parameters
-    n_cells=200,  # Spatial resolution
+    n_cells=500,  # Spatial resolution
     ng=3,  # Ghost cells
     bounds=(bounds[4], bounds[5]),  # Domain length (meters)
     CFL=0.9,  # Stability factor
     t_end=20.0,  # Simulation duration
 
     # Initial Conditions
-    p0_inlet=3.5e6,  # 3.5 MPa Chamber Pressure
     p_inf=100.0e3,  # 1 atm Ambient Pressure
 
-    rho_p=1600.0,  # Propellant Density
-    a_coef=0.000035,
+    rho_p=1800.0,  # Propellant Density
+    a_coef=0.00678/(6.9E6**0.360),
     n_exp=0.36,
     br_initial=10.0e-3,
+
+    Tf = 2713+273.15,
+
+    R = 315,
+    gamma = 1.14,
+
+    log_interval = 1000
+
 
 )
 
 ls_config = level_set_model.config.SimulationConfig(
-    n_periodics=11,  # Number of symmetric segments
+    n_periodics=6,  # Number of symmetric segments
     size=(50, 40, 300),  # Resolution: (nr, ntheta, nz)
     bounds=bounds,  # Physical dimensions
     file_scale=1.0e-3,
@@ -39,7 +46,7 @@ ls_config = level_set_model.config.SimulationConfig(
     file_case=case_file,  # Casing SDF input
 
     ng=3,  # Ghost cells
-    CFL=0.8,  # Stability factor
+    CFL=0.3,  # Stability factor
     t_end=20.0,  # Simulation duration
     br_initial=10.0e-3  # Initial burn rate
 )
@@ -47,7 +54,7 @@ ls_config = level_set_model.config.SimulationConfig(
 coupled_conf = coupled_solver_model.config.CoupledConfig(
     ib_config=ib_config,
     ls_config=ls_config,
-    t_end=1)
+    t_end=5.0)
 
 solver = coupled_solver_model.solver.CoupledSolver(coupled_conf)
 
@@ -75,16 +82,6 @@ while solver.t < coupled_conf.t_end:
     })
     history.append(df_step)
 
-    # ========================================================
-    # DEBUGGING: Check for bad areas
-
-    areas  = solver.ls.state.A_flow
-    z_distances = solver.ls.state.x
-
-
-    # ========================================================
-
-
     print(f"Time: {t_current:.4f} s | dt_ls: {dt_ls:.2e} | P_head: {p_head / 1e6:.2f} MPa |")
 
     if t_current >= coupled_conf.t_end or p_head < ib_config.p_inf:
@@ -93,9 +90,6 @@ while solver.t < coupled_conf.t_end:
 
 final_surface = solver.ls.grid.pv_grid.contour(scalars="propellant", isosurfaces=[0.0])
 
-# z=880*1E-3
-# initial_surface = initial_surface.slice(normal='z', origin=(0, 0, z))
-# final_surface = final_surface.slice(normal='z', origin=(0, 0, z))
 
 b=final_surface.bounds
 n=ls_config.size[2]/2
