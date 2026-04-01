47f6f1cea0e0ddd0e72d73ec76a45edb476a9df1
brandon.schroder <bs@mkaerospace.co.za>
2026-04-01 10:43:40 +0200

    Rework burn_rate.py for njit compatible burn rate models

--- internal_ballistics_model/burn_rate.py
+++ internal_ballistics_model/burn_rate.py
@@ -3,37 +3,35 @@ from enum import IntEnum
 
 from .config import *
 
+@njit(cache=True)
+def st_roberts(pressure, coefficient, exponent):
+    base_burn_rate = coefficient * pressure ** exponent
+    return base_burn_rate
 
-def lenoir_robillard(base_burn_rate, config: SimulationConfig, state: FlowState):
-    alpha = 0.92E-5
-    beta = 53.0
+@njit(cache=True)
+def override(base_burn_rate, br_initial):
+    burn_rate = br_initial * np.ones_like(base_burn_rate)
+    return burn_rate
 
-    u = np.maximum(abs(state.u), 1e-7)
+@njit(cache=True)
+def lenoir_robillard(base_burn_rate, alpha, beta, u, rho, A, P_wetted, rho_p):
+    u = np.maximum(np.abs(u), 1e-7)
 
-    Dh = 4 * state.A / state.P_wetted
-    G = state.rho * u
-    burn_rate = base_burn_rate + alpha * ((G ** 0.8) / (Dh ** 0.2)) * np.exp(-(beta * base_burn_rate * config.rho_p) / G)
+    Dh = 4 * A / P_wetted
+    G = np.abs(rho * u)
+
+    burn_rate = base_burn_rate + alpha * ((G ** 0.8) / (Dh ** 0.2)) * np.exp(-(beta * base_burn_rate * rho_p) / G)
 
     return burn_rate
 
+@njit(cache=True)
+def mukunda_paul(base_burn_rate, mu, K1, K2, m, gth, u, rho, A, P_wetted, rho_p):
+    u = np.maximum(np.abs(u), 1e-7)
 
-def mukunda_paul(base_burn_rate, config: SimulationConfig, state: FlowState):
-    rho_p = config.rho_p
-    rho = state.rho
-    u = np.maximum(abs(state.u), 1e-7)
+    Dh = 4 * A / P_wetted
+    G = np.abs(rho * u)
 
-    mf_port = rho * u * state.A
-
-    mu = 8.85E-5
-    K1 = 0.023
-    K2 = 2.3714 # DV (2.820054) # Correct (2.3714)
-    m = -0.125
-    gth = 35.0
-
-    Dh = 4 * state.A / state.P_wetted
     Re0 = rho_p * base_burn_rate * Dh / mu
-
-    G = abs(mf_port) / state.A
     g0 = G / (rho_p * base_burn_rate)
     g = g0 * K2 * Re0 ** m
 
@@ -45,26 +43,40 @@ def mukunda_paul(base_burn_rate, config: SimulationConfig, state: FlowState):
 
     return burn_rate
 
+
 class BurnModel(IntEnum):
     NONE = 0
     OVERRIDE = 1
     LR = 2
     MP = 3
 
-def burn_rate_model(config: SimulationConfig, state: FlowState, model_flag):
-    coefficient = config.a_coef
-    exponent = config.n_exp
 
-    base_burn_rate = coefficient * state.p ** exponent
+def burn_rate_model(config: SimulationConfig, state: FlowState, model_flag):
+
+    base_burn_rate = st_roberts(state.p, config.a_coef, config.n_exp)
 
     if model_flag==BurnModel.OVERRIDE.value:
-        burn_rate = config.br_initial * np.ones_like(state.p)
+        burn_rate = override(base_burn_rate, config.br_initial)
 
     elif model_flag==BurnModel.LR.value:
-        burn_rate = lenoir_robillard(base_burn_rate, config, state)
+        alpha = getattr(config, "lr_alpha", 1E-5)
+        beta = getattr(config, "lr_beta", 53)
+        burn_rate = lenoir_robillard(base_burn_rate,
+                                     alpha, beta,
+                                     state.u, state.rho, state.A, state.P_wetted,
+                                     config.rho_p)
 
     elif model_flag==BurnModel.MP.value:
-        burn_rate = mukunda_paul(base_burn_rate, config, state)
+        mu =  getattr(config, "mp_mu",     8.85E-5)
+        K1 =  getattr(config, "mp_k1",  0.023)
+        K2 =  getattr(config, "mp_k2",  2.3714)
+        m  =  getattr(config, "mp_m",   -0.125)
+        gth = getattr(config, "mp_gth", 35.0)
+
+        burn_rate = mukunda_paul(base_burn_rate,
+                                 mu, K1, K2, m, gth,
+                                 state.u, state.rho, state.A, state.P_wetted,
+                                 config.rho_p)
 
     else:
         burn_rate=base_burn_rate
