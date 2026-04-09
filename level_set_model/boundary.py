d2817275bc232ed33fa356e3e387acb3e343fb71
brandon.schroder <bs@mkaerospace.co.za>
2026-04-09 15:50:51 +0200

    Fix extrapolative boundary condition

--- level_set_model/boundary.py
+++ level_set_model/boundary.py
@@ -4,19 +4,21 @@ from enum import IntEnum
 
 @njit(cache=True)
 def apply_extrapolative_bc(phi, ng):
-    phi[:ng, :, :]     = 2 * phi[ng:ng+1, :, :]     - phi[ng+1:2*ng+1, :, :][::-1, :, :]
-    phi[-ng:, :, :]    = 2 * phi[-ng-1:-ng-2:-1, :, :] - phi[-ng-2:-2*ng-2:-1, :, :][::-1, :, :]
+    for i in range(ng):
+        phi[i, :, :] = phi[ng, :, :]
+        phi[-i-1, :, :] = phi[-ng-1, :, :]
 
-    # z boundaries
-    phi[:, :, :ng]     = 2 * phi[:, :, ng:ng+1]     - phi[:, :, ng+1:2*ng+1][:, :, ::-1]
-    phi[:, :, -ng:]    = 2 * phi[:, :, -ng-1:-ng-2:-1] - phi[:, :, -ng-2:-2*ng-2:-1][:, :, ::-1]
+    for k in range(ng):
+        phi[:, :, k] = phi[:, :, ng]
+        phi[:, :, -k-1] = phi[:, :, -ng-1]
 
     return phi
 
 
 @njit(cache=True)
-def apply_periodic_bc(phi):
+def apply_periodic_bc(phi, ng):
     phi[:, -1, :] = phi[:, 0, :]
+
     return phi
 
 class BCType(IntEnum):
@@ -26,7 +28,7 @@ class BCType(IntEnum):
 def apply_boundary_conditions(phi, ng, bc_type):
     if bc_type == BCType.DEFAULT:
 
+        phi = apply_periodic_bc(phi, ng)
         phi = apply_extrapolative_bc(phi, ng)
-        phi = apply_periodic_bc(phi)
 
     return phi
